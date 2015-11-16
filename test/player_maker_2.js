ac5d4c1ed7a6519927510ee3427385f1b35a5c89
Joechen1210 <joes9630003@gmail.com>
2015-11-16 14:37:25 +0800

    Update player_maker_2.js

--- test/player_maker_2.js
+++ test/player_maker_2.js
@@ -139,9 +139,9 @@
       duration = player.duration();
     });
 
-    clientRect = offsetParent(progressControl.el()).getBoundingClientRect();
-    right = (clientRect.width || clientRect.right) + pageXOffset;
-    var num = 8 / duration * clientRect.width;
+    //clientRect = offsetParent(progressControl.el()).getBoundingClientRect();
+    //right = (clientRect.width || clientRect.right) + pageXOffset;
+    //var num = 8 / duration * clientRect.width;
     
     makerpoint.style.left = right + duration ;
     
