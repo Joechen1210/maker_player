0239a030b3c5b46547b31f6985ab91fcfcf4051f
Joechen1210 <joes9630003@gmail.com>
2015-11-16 14:32:14 +0800

    Update player_maker_2.js

--- test/player_maker_2.js
+++ test/player_maker_2.js
@@ -143,7 +143,7 @@
     right = (clientRect.width || clientRect.right) + pageXOffset;
     var num = 8 / duration * clientRect.width;
     
-    makerpoint.style.left = duration ;
+    makerpoint.style.left = right + duration ;
     
     // add the thumbnail to the player
     
