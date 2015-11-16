e548838928dc5b854a3da7af3dadd04dcbb7a84a
Joechen1210 <joes9630003@gmail.com>
2015-11-16 17:49:07 +0800

    Update player_maker_2.js

--- test/player_maker_2.js
+++ test/player_maker_2.js
@@ -141,7 +141,7 @@
     var sec_num = parseInt(duration, 10);
     makerpoint.style.left = time + '%';
     
-    var dutext = document.getElementsByClassName("vjs-duration-display").innerText;;
+    var dutext = document.getElementsByClassName("vjs-duration-display").innerText;
     
     makerpoint.onclick = function()
     {
