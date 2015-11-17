38f32e4fae51ae26ce42af36e4854510621006ad
Joechen1210 <joes9630003@gmail.com>
2015-11-17 14:22:45 +0800

    Update player_maker_2.js

--- test/player_maker_2.js
+++ test/player_maker_2.js
@@ -106,7 +106,7 @@
     {
       player.currentTime(8);
      // x.innerHTML = "0.08";
-     alert(" sec: " + sec_num + " type: " + typeof(sec_num) + "  time: " + time + " type: " + typeof(time) + " settingstime:  "+ settings[0]);
+     alert(" sec: " + sec_num + " type: " + typeof(sec_num) + "  time: " + time + " type: " + typeof(time) + " settingstime:  "+ settings.el().length);
     }
     progressControl.el().appendChild(makerpoint);
     
