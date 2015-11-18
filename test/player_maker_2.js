d63278e6e5770e4e1ef7a452bac7fdecd0ec60b8
Joechen1210 <joes9630003@gmail.com>
2015-11-18 17:39:14 +0800

    Update player_maker_2.js

--- test/player_maker_2.js
+++ test/player_maker_2.js
@@ -158,7 +158,7 @@
     // center the thumbnail over the cursor if an offset wasn't provided
     if (!img.style.left && !img.style.right) {
       img.onload = function() {
-        img.style.left = (img.naturalWidth / 2) + 'px';
+        img.style.right = (img.naturalWidth / 2) + 'px';
       };
     }
   
