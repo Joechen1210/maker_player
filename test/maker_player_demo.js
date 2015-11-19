efc909b603cad59b3a07042f316c62df06e7e453
Joechen1210 <joes9630003@gmail.com>
2015-11-19 10:07:03 +0800

    Create maker_player_demo.js

---
+++ test/maker_player_demo.js
@@ -0,0 +1,85 @@
+(function() {
+  var defaults = {
+      0: {
+        src: 'example-thumbnail.png'
+      }
+    },
+    extend = function() {
+      var args, target, i, object, property;
+      args = Array.prototype.slice.call(arguments);
+      target = args.shift() || {};
+      for (i in args) {
+        object = args[i];
+        for (property in object) {
+          if (object.hasOwnProperty(property)) {
+            if (typeof object[property] === 'object') {
+              target[property] = extend(target[property], object[property]);
+            } else {
+              target[property] = object[property];
+            }
+          }
+        }
+      }
+      return target;
+    };
+
+  /**
+   * register the thubmnails plugin
+   */
+  videojs.plugin('makerpoint', function(options) {
+    var div, settings, img, player, progressControl, duration, moveListener, moveCancel, du, num, time, crright;
+    settings = extend({}, defaults, options);
+    player = this;
+    progressControl = player.controlBar.progressControl;
+    progressControl.el().style.position = 'relative';
+    
+    player.ready(function(){
+    settings = extend({}, defaults, options);
+    
+    for(var i = 0; i < Object.keys(settings).length; i++)
+    {
+    progressControl = player.controlBar.progressControl;
+    time = settings[i].time;
+    
+    makerpoint = document.createElement('div');
+    makerpoint.className = 'vjs-makerpoint ';
+    makerpoint.className += 'vjs-makerpoint-' + time;
+    makerpoint.id = time;
+    makerbt = document.createElement('button');
+    makerbt.className = 'vjs-makerbt';
+    
+    
+    sec_num = player.duration();
+    time1 = (time/Math.floor(sec_num))*100;
+    makerpoint.style.left = time1 + '%';
+    
+    // when the container is MP4
+    player.on('durationchange', function(event) {
+      sec_num = player.duration();
+      time1 = (time/Math.floor(sec_num))*100;
+      makerpoint.style.left = time1 + '%';
+    });
+
+    // when the container is HLS
+    player.on('loadedmetadata', function(event) {
+      sec_num = player.duration();
+      time1 = (time/Math.floor(sec_num))*100;
+      makerpoint.style.left = time1 + '%';
+    });
+    
+    makerpoint.addEventListener('click', makerclickevent, false);
+    
+    progressControl.el().appendChild(makerpoint);
+    
+    }
+    
+     function makerclickevent(e) {
+     var clickedItem = e.target.id;
+     var t = parseInt(clickedItem);
+     player.currentTime(t);
+     //alert("Hello " + clickedItem);
+     }
+    
+    });
+  });
+})();
