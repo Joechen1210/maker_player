a8bd417902fbc667b8d46aa11e35bb29ce04c2b5
Joechen1210 <joes9630003@gmail.com>
2015-11-19 11:29:51 +0800

    Update maker_player_demo.js

--- test/maker_player_demo.js
+++ test/maker_player_demo.js
@@ -27,7 +27,7 @@
    * register the thubmnails plugin
    */
   videojs.plugin('makerpoint', function(options) {
-    var div, settings, img, player, progressControl, duration, moveListener, moveCancel, du, num, time, crright;
+    var div, settings, img, player, progressControl, duration, moveListener, moveCancel, du, num, time, makerclassname, makerpoint;
     settings = extend({}, defaults, options);
     player = this;
     progressControl = player.controlBar.progressControl;
@@ -47,11 +47,10 @@
     
     makerpoint = document.createElement('div');
     makerpoint.className = 'vjs-makerpoint ';
-    makerpoint.className += 'vjs-makerpoint-' + time;
+    makerpoint.className += 'vjs-makerpoint-' + i;
     makerpoint.id = time;
     makerbt = document.createElement('button');
     makerbt.className = 'vjs-makerbt';
-    
     
     sec_num = player.duration();
     time1 = (time/Math.floor(sec_num))*100;
@@ -59,28 +58,37 @@
     
     console.log(" No: " + i + " duration1: " + sec_num + " left1: " + time1);
     
-    // when the container is MP4
-    player.on('durationchange', function(event) {
-      sec_num = player.duration();
-      time1 = (time/Math.floor(sec_num))*100;
-      makerpoint.style.left = time1 + '%';
-      console.log(" No: " + i +  " duration2: " + sec_num + " left2: " + time1);
-    });
-
-    // when the container is HLS
-    player.on('loadedmetadata', function(event) {
-      sec_num = player.duration();
-      time1 = (time/Math.floor(sec_num))*100;
-      makerpoint.style.left = time1 + '%';
-      console.log( " No: " + i + " duration3: " + sec_num + " left3: " + time1);
-    });
-    
     makerpoint.addEventListener('click', makerclickevent, false);
     
     progressControl.el().appendChild(makerpoint);
     
     }
     
+     // when the container is MP4
+    player.on('durationchange', function(event) {
+      var makerpoint;
+       for(var i = 0; i < Object.keys(settings).length; i++)
+    {
+      makerpoint = document.getElementById(i);
+      sec_num = player.duration();
+      time1 = (time/Math.floor(sec_num))*100;
+      makerpoint.style.left = time1 + '%';
+      console.log(" No: " + i +  " duration2: " + sec_num + " left2: " + time1);
+    }
+    });
+
+    // when the container is HLS
+    player.on('loadedmetadata', function(event) {
+      var makerpoint;
+       for(var i = 0; i < Object.keys(settings).length; i++)
+    {
+      makerpoint = document.getElementById(i);
+      sec_num = player.duration();
+      time1 = (time/Math.floor(sec_num))*100;
+      makerpoint.style.left = time1 + '%';
+      console.log( " No: " + i + " duration3: " + sec_num + " left3: " + time1);
+    }
+    });
      function makerclickevent(e) {
      var clickedItem = e.target.id;
      var t = parseInt(clickedItem);
