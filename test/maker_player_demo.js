af34d778c51cbfd8a477dfbea53c75e58f0dbf24
Joechen1210 <joes9630003@gmail.com>
2015-11-19 11:20:41 +0800

    Update maker_player_demo.js

--- test/maker_player_demo.js
+++ test/maker_player_demo.js
@@ -57,24 +57,22 @@
     time1 = (time/Math.floor(sec_num))*100;
     makerpoint.style.left = time1 + '%';
     
-    console.log( " duration1: " + sec_num + " left1: " + time1);
+    console.log(" No: " + i + " duration1: " + sec_num + " left1: " + time1);
     
     // when the container is MP4
     player.on('durationchange', function(event) {
       sec_num = player.duration();
       time1 = (time/Math.floor(sec_num))*100;
       makerpoint.style.left = time1 + '%';
-      console.log( " duration2: " + sec_num + " left2: " + time1);
+      console.log(" No: " + i +  " duration2: " + sec_num + " left2: " + time1);
     });
-
-
 
     // when the container is HLS
     player.on('loadedmetadata', function(event) {
       sec_num = player.duration();
       time1 = (time/Math.floor(sec_num))*100;
       makerpoint.style.left = time1 + '%';
-      console.log( " duration3: " + sec_num + " left3: " + time1);
+      console.log( " No: " + i + " duration3: " + sec_num + " left3: " + time1);
     });
     
     makerpoint.addEventListener('click', makerclickevent, false);
